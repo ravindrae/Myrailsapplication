d3d53da1a759c16e6beee11bb83d6e5106e78543
ravindrae <ravindraleader@gmail.com>
2018-06-26 09:34:51 +0530

    updating

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home' => 'static_pages#home'
+  get 'static_pages/help' =>'static_pages#help'
+  get 'static_pages/about' => 'static_pages#about'
+  resources :microposts
+  resources :users
+  root to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
