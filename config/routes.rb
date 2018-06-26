961118b45d3326845eedd8a6d634a2df81620e77
ravindrae <ravindraleader@gmail.com>
2018-06-26 16:13:10 +0530

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home' => 'static_pages#home'
-  get 'static_pages/help' =>'static_pages#help'
-  get 'static_pages/about' => 'static_pages#about'
-  resources :microposts
+  get 'sessions/new'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get '/signup',to:'users#new'
+  root to: "static_pages#home"
+
   resources :users
-  root to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
